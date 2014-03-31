44d140a3a400632af8f563d5e70137371b9ce088
xero <simon.beckstein@gmail.com>
2014-03-31 19:54:31 +0200

    added paning

--- core/src/main/java/de/cwclan/gdxtest/core/games/TiledGame.java
+++ core/src/main/java/de/cwclan/gdxtest/core/games/TiledGame.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -38,9 +39,8 @@ public class TiledGame implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.position.set(player.getX(), player.getY(), 0);
-        camera.update();
-
+      //  camera.position.set(player.getX(), player.getY(), 0);
+        // camera.update();
         renderer.setView(camera);
         renderer.render();
 
@@ -84,7 +84,15 @@ public class TiledGame implements Screen {
                 return true;
             }
 
-        }, player.getInputProcessor()));
+           
+        }, new GestureDetector(new GestureDetector.GestureAdapter() {
+            @Override
+            public boolean pan(float x, float y, float deltaX, float deltaY) {
+                camera.translate(-deltaX, deltaY);
+                camera.update();
+                return false;
+            }
+        }), player.getInputProcessor()));
 
     }
 
